78bf89efdd4b3aab136660d9d746a4d4ca44ec35
rahul-dewangan-10 <121477559+rahul-dewangan-10@users.noreply.github.com>
2024-07-11 18:04:39 +0530

    Add files via upload

--- SQL QUERIES .sql
+++ SQL QUERIES .sql
@@ -98,8 +98,9 @@ ADD downtime_num INT AS (
 );
 
 
--- have already did same calculation for spindle speed(rpm)
+-- trying to find the range for each perameter with most percentage of machine failures 
 select max(`Spindle_Vibration(Âµm)`) from md_data 
+select min(`Spindle_Vibration(Âµm)`) from md_data 
 select count(*) from md_data where `Spindle_Vibration(Âµm)` >0 and  `Spindle_Vibration(Âµm)` <0.3  and downtime = "machine_failure"; 
 -- out of 1231 failures,(no. of failures)  when spindle vibration is between() ,
 -- 0 out of 1 when -0.5 to 0 (safe range but not feasible),   
@@ -121,4 +122,81 @@ select sum(downtime_num) from md_data where  `Spindle_Vibration(Âµm)` >0 and  
 
 select sum(downtime_num) from md_data where`Spindle_Vibration(Âµm)`> 25100;
 
+SELECT * FROM md_data
 
+SELECT 
+    COUNT(*) AS total_failures,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN -14.326454 AND 101.184167 THEN 1 ELSE 0 END) AS hydraulic_pressure_failures,
+    SUM(CASE WHEN `Coolant_Pressure(bar)` BETWEEN 0.325 AND 4.955532 THEN 1 ELSE 0 END) AS coolant_pressure_failures,
+    SUM(CASE WHEN `Air_System_Pressure(bar)` BETWEEN 5.063480 AND 6.499094 THEN 1 ELSE 0 END) AS air_system_pressure_failures,
+    SUM(CASE WHEN `Coolant_Temperature` BETWEEN 4.100000 AND 18.518774 THEN 1 ELSE 0 END) AS coolant_temperature_failures,
+    SUM(CASE WHEN `Hydraulic_Oil_Temperature(Â°C)` BETWEEN 35.200000 AND 47.616422 THEN 1 ELSE 0 END) AS hydraulic_oil_temperature_failures,
+    SUM(CASE WHEN `Spindle_Bearing_Temperature(Â°C)` BETWEEN 22.600000 AND 35.064763 THEN 1 ELSE 0 END) AS spindle_bearing_temperature_failures,
+    SUM(CASE WHEN `Spindle_Vibration(Âµm)` BETWEEN -0.461000 AND 1.007646 THEN 1 ELSE 0 END) AS spindle_vibration_failures,
+    SUM(CASE WHEN `Tool_Vibration(Âµm)` BETWEEN 3.469000 AND 25.415098 THEN 1 ELSE 0 END) AS tool_vibration_failures,
+    SUM(CASE WHEN `Spindle_Speed(RPM)` BETWEEN 0.000000 AND 20271.568249 THEN 1 ELSE 0 END) AS spindle_speed_failures,
+    SUM(CASE WHEN `Voltage(volts)` BETWEEN 202.000000 AND 349.101638 THEN 1 ELSE 0 END) AS voltage_failures,
+    SUM(CASE WHEN `Torque(Nm)` BETWEEN 0 AND 25.196960 THEN 1 ELSE 0 END) AS torque_failures,
+    SUM(CASE WHEN `Cutting(kN)` BETWEEN 1.800000 AND 2.785586 THEN 1 ELSE 0 END) AS cutting_failures
+FROM md_data
+WHERE downtime = 'machine_failure';
+
+
+SELECT 
+    COUNT(*) AS total_failures,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN -14.326454 AND 101.184167 THEN 1 ELSE 0 END) AS hydraulic_pressure_failures,
+    SUM(CASE WHEN `Coolant_Pressure(bar)` BETWEEN 0.325 AND 4.955532 THEN 1 ELSE 0 END) AS coolant_pressure_failures,
+    SUM(CASE WHEN `Air_System_Pressure(bar)` BETWEEN 5.063480 AND 6.499094 THEN 1 ELSE 0 END) AS air_system_pressure_failures,
+    SUM(CASE WHEN `Coolant_Temperature` BETWEEN 4.100000 AND 18.518774 THEN 1 ELSE 0 END) AS coolant_temperature_failures,
+    SUM(CASE WHEN `Hydraulic_Oil_Temperature(Â°C)` BETWEEN 35.200000 AND 47.616422 THEN 1 ELSE 0 END) AS hydraulic_oil_temperature_failures,
+    SUM(CASE WHEN `Spindle_Bearing_Temperature(Â°C)` BETWEEN 22.600000 AND 35.064763 THEN 1 ELSE 0 END) AS spindle_bearing_temperature_failures,
+    SUM(CASE WHEN `Spindle_Vibration(Âµm)` BETWEEN -0.461000 AND 1.007646 THEN 1 ELSE 0 END) AS spindle_vibration_failures,
+    SUM(CASE WHEN `Tool_Vibration(Âµm)` BETWEEN 3.469000 AND 25.415098 THEN 1 ELSE 0 END) AS tool_vibration_failures,
+    SUM(CASE WHEN `Spindle_Speed(RPM)` BETWEEN 0.000000 AND 20271.568249 THEN 1 ELSE 0 END) AS spindle_speed_failures,
+    SUM(CASE WHEN `Voltage(volts)` BETWEEN 202.000000 AND 349.101638 THEN 1 ELSE 0 END) AS voltage_failures,
+    SUM(CASE WHEN `Torque(Nm)` BETWEEN 0 AND 25.196960 THEN 1 ELSE 0 END) AS torque_failures,
+    SUM(CASE WHEN `Cutting(kN)` BETWEEN 1.800000 AND 2.785586 THEN 1 ELSE 0 END) AS cutting_failures
+FROM md_data
+WHERE downtime = 'no_machine_failure';
+
+
+-- for perameters in their minimum to avg range
+-- total_failures, hydraulic_pressure_failures, coolant_pressure_failures, air_system_pressure_failures, coolant_temperature_failures, hydraulic_oil_temperature_failures, spindle_bearing_temperature_failures, spindle_vibration_failures, tool_vibration_failures, spindle_speed_failures, voltage_failures, torque_failures, cutting_failures
+-- machine_filure case = '1231', '1046', '841', '605', '510', '621', '619', '637', '623', '634', '637', '737', '578' ;
+-- no machine failure case = '1150', '289', '362', '573', '524', '568', '558', '556', '565', '591', '568', '574', '628' ;
+
+
+-- for  perameters when their range is between mean and maximum 
+-- total_failures, hydraulic_pressure_failures, coolant_pressure_failures, air_system_pressure_failures, coolant_temperature_failures, hydraulic_oil_temperature_failures, spindle_bearing_temperature_failures, spindle_vibration_failures, tool_vibration_failures, spindle_speed_failures, voltage_failures, torque_failures, cutting_failures
+-- Machine failure case = '1231', '185', '390', '626', '721', '610', '612', '594', '608', '597', '594', '494', '653';
+-- No machine failure case = '1150', '861', '788', '577', '626', '582', '592', '594', '585', '559', '582', '576', '522';
+
+
+
+-- finding range of hydrolic pressure whith most %age of machine failures
+SELECT 
+    count(*) as total_failures_hydrolic_pressure,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 50 AND 100 THEN 1 ELSE 0 END) AS in_range_50_to_100,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 100 AND 150 THEN 1 ELSE 0 END) AS in_range_100_to_150,
+	SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 150 AND 200 THEN 1 ELSE 0 END) AS in_range_150_to_200
+ from md_data
+ 
+union all 
+
+SELECT 
+    count(*) as total_failures_hydrolic_pressure,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 50 AND 100 THEN 1 ELSE 0 END) AS in_range_50_to_100,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 100 AND 150 THEN 1 ELSE 0 END) AS in_range_100_to_150,
+	SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 150 AND 200 THEN 1 ELSE 0 END) AS in_range_150_to_200
+ from md_data as machine_failure
+ where downtime = "machine_failure"
+
+  
+union all 
+SELECT 
+    count(*) as total_failures_hydrolic_pressure,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 50 AND 100 THEN 1 ELSE 0 END) AS in_range_50_to_100,
+    SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 100 AND 150 THEN 1 ELSE 0 END) AS in_range_100_to_150,
+	SUM(CASE WHEN `Hydraulic_Pressure(bar)` BETWEEN 150 AND 200 THEN 1 ELSE 0 END) AS in_range_150_to_200
+ from md_data as no_machine_failure
+ where downtime = "no_machine_failure";
